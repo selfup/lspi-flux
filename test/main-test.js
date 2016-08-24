5877fb0c25bdc16403493d9bf8156b92e3456302
Regis <boudinot.regis@yahoo.com>
2016-08-23 22:37:31 -0600

    changing up the API

--- test/main-test.js
+++ test/main-test.js
@@ -4,12 +4,18 @@ const RejsStore = require('../lib/main')
 
 describe('lspi as db for store', function () {
 
-  beforeEach(() => {
-    this.store = new RejsStore()
+  it('should load defualts correctly', () => {
+    this.scopedStore = new RejsStore()
+
+    assert.deepEqual(this.scopedStore.init({}).state, {})
+    assert.deepEqual(this.scopedStore.mainStore, {})
+    assert.deepEqual(this.scopedStore.fetchState().state, {})
   })
 
-  it('should load correctly', () => {
-    assert.deepEqual(this.store.mainStore, {})
+  it('should be able to store valid Object Literals', () => {
+    this.scopedStore = new RejsStore({"ok": "wow"})
+
+    assert.deepEqual(this.scopedStore.mainStore, {"ok": "wow"})
   })
 
 })
