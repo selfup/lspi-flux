f53d6f43585c1cb1ac60ffc5eeab4665d357ea02
Regis <boudinot.regis@yahoo.com>
2016-08-23 22:57:38 -0600

    Add behaviour to the tests

--- test/main-test.js
+++ test/main-test.js
@@ -4,18 +4,47 @@ const RejsStore = require('../lib/main')
 
 describe('lspi as db for store', function () {
 
-  it('should load defualts correctly', () => {
+  it('should load default state correctly', () => {
     this.scopedStore = new RejsStore()
 
-    assert.deepEqual(this.scopedStore.init({}).state, {})
+    assert.deepEqual(this.scopedStore.setState({}).state, {})
+    assert.deepEqual(this.scopedStore.setState({}).status, true)
+
     assert.deepEqual(this.scopedStore.mainStore, {})
+    
     assert.deepEqual(this.scopedStore.fetchState().state, {})
+    assert.deepEqual(this.scopedStore.fetchState().status, true)
+  })
+
+  it('should have truthy status returns', () => {
+    this.scopedStore = new RejsStore()
+
+    assert.deepEqual(this.scopedStore.setState({}).status, true)
+    assert.deepEqual(this.scopedStore.fetchState().status, true)
   })
 
   it('should be able to store valid Object Literals', () => {
     this.scopedStore = new RejsStore({"ok": "wow"})
 
     assert.deepEqual(this.scopedStore.mainStore, {"ok": "wow"})
+  })
+
+  it('feels fairly natural to use the API', () => {
+    this.scopedStore = new RejsStore({"ok": "wow"})
+
+    assert.deepEqual(this.scopedStore.mainStore, {"ok": "wow"})
+
+    let appState = {main: {}}
+    const currentState = this.scopedStore.fetchState()
+
+    if (currentState.status) appState.main = currentState.state
+    if (!currentState.status) {
+      /* handle error and pick to either modify state with old state or keep it as is */
+      /* example below on how to reset state to old state provided by the API if you want to */
+      appState.main = currentState.state
+    }
+
+    assert.deepEqual(appState, {main: {"ok": "wow"}})
   })
 
 })
